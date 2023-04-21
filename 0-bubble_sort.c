419da4501c06f6dc687b8e45c3324aa9d49d249e
tonytec <tonyexplorer202@gmail.com>
2023-04-21 17:22:29 +0100

    Add bubble_sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+void swap(int *a, int *b);
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: nothing
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t index_1, index_2;
+
+	if (size < 2)
+		return;
+
+	index_1 = 0;
+
+	for (; array != NULL && index_1 < size; index_1++)
+	{
+		index_2 = 0;
+
+		for (; index_2 < (size - 1); index_2++)
+		{
+			if (array[index_2] > array[index_2 + 1])
+			{
+				swap(&array[index_2], &array[index_2 + 1]);
+				print_array(array, size);
+			}
+		}
+
+	}
+}
+
+/**
+ * swap - swap two integer
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: nothing
+*/
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
